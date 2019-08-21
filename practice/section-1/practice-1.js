22d9f3a92a86380be7528c247a1c3b71c7341e52
zhangyiran01 <1183920942@qq.com>
2019-08-21 10:27:50 +0800

    zhangyiran01

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+  let result;
+  for(let i=0;i<collection.length;i++){
+    if(collection[i]===element){
+      return collection[i];
+    }
+  }
+
+  return '查无此数';
 }
